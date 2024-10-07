81a17f84e06020e9aa23a707bfc69a8893d67ef8
Kayla Radu <82993098+KaylaR2040@users.noreply.github.com>
2024-10-07 18:56:23 -0400

    Update main.c

--- Project05/Project03 cc/main.c
+++ Project05/Project03 cc/main.c
@@ -27,7 +27,7 @@ void Display_Process(void);
 void Init_LEDs(void);
 void Carlson_StateMachine(void);
 
-  // Global Variables
+// Global Variables
 volatile char slow_input_down;
 extern char display_line[4][11];
 extern char *display[4];
@@ -61,118 +61,113 @@ extern volatile unsigned int instruction = 0;
 void main(void){
     event = NONE;
     state = WAIT;
-//    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    //    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
 
-//------------------------------------------------------------------------------
-// Main Program
-// This is the main routine for the program. Execution of code starts here.
-// The operating system is Back Ground Fore Ground.
-//
-//------------------------------------------------------------------------------
-  PM5CTL0 &= ~LOCKLPM5;
-// Disable the GPIO power-on default high-impedance mode to activate
-// previously configured port settings
-  Init_Ports();                        // Initialize Ports
-  Init_Clocks();                       // Initialize Clock System
-  Init_Conditions();                   // Initialize Variables and Initial Conditions
-  Init_Timers();                       // Initialize Timers
-  Init_LCD();                          // Initialize LCD
-// Place the contents of what you want on the display, in between the quotes
-// Limited to 10 characters per line
-  strcpy(display_line[0], "   NCSU   ");
-  strcpy(display_line[1], " WOLFPACK ");
-  strcpy(display_line[2], "  ECE306  ");
-  strcpy(display_line[3], "  GP I/O  ");
-  display_changed = TRUE;
-  Display_Update(0,0,0,0);
-  display_changed = FALSE;
+    //------------------------------------------------------------------------------
+    // Main Program
+    // This is the main routine for the program. Execution of code starts here.
+    // The operating system is Back Ground Fore Ground.
+    //
+    //------------------------------------------------------------------------------
+    PM5CTL0 &= ~LOCKLPM5;
+    // Disable the GPIO power-on default high-impedance mode to activate
+    // previously configured port settings
+    Init_Ports();                        // Initialize Ports
+    Init_Clocks();                       // Initialize Clock System
+    Init_Conditions();                   // Initialize Variables and Initial Conditions
+    Init_Timers();                       // Initialize Timers
+    Init_LCD();                          // Initialize LCD
+    // Place the contents of what you want on the display, in between the quotes
+    // Limited to 10 characters per line
+    strcpy(display_line[0], "   NCSU   ");
+    strcpy(display_line[1], " WOLFPACK ");
+    strcpy(display_line[2], "  ECE306  ");
+    strcpy(display_line[3], "  GP I/O  ");
+    display_changed = TRUE;
+    Display_Update(0,0,0,0);
+    display_changed = FALSE;
 
-  wheel_move = 0;
-  forward = TRUE;
+    wheel_move = 0;
+    forward = TRUE;
 
-  backlight_changed = TRUE;
-  backlight = TRUE;
-  Backlight_Process();               // Turn Backlight on/off
-
-
-//------------------------------------------------------------------------------
-// Beginning of the "While" Operating System
-//------------------------------------------------------------------------------
-  while(ALWAYS) {                      // Can the Operating system run
-
-      Switches_Process();                // Check for switch state change
-//      Carlson_StateMachine();            // Run a Time Based State Machine
-      Display_Process();                 // Update Display
-      P3OUT ^= TEST_PROBE;               // Change State of TEST_PROBE OFF
-
-      // Time Sequence Handling
-      if(Last_Time_Sequence != Time_Sequence){
-          Last_Time_Sequence = Time_Sequence;
-          cycle_time++;
-          time_change = 1;            // Flag to indicate a time change occurred
-      }
-
-      Wheel_Move();
-      motorDirec();
-//      if(!instruction){
-//      Display_Changing();
-//      }
-
-      }
-      // This will make it move
+    backlight_changed = TRUE;
+    backlight = TRUE;
+    Backlight_Process();               // Turn Backlight on/off
 
 
-//------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------
+    // Beginning of the "While" Operating System
+    //------------------------------------------------------------------------------
+    while(ALWAYS) {                      // Can the Operating system run
+
+        Switches_Process();                // Check for switch state change
+        //      Carlson_StateMachine();            // Run a Time Based State Machine
+        Display_Process();                 // Update Display
+        P3OUT ^= TEST_PROBE;               // Change State of TEST_PROBE OFF
+
+        // Time Sequence Handling
+        if(Last_Time_Sequence != Time_Sequence){
+            Last_Time_Sequence = Time_Sequence;
+            cycle_time++;
+            time_change = 1;            // Flag to indicate a time change occurred
+        }
+
+        Wheel_Move();
+        motorDirec();
+        //      if(!instruction){
+        //      Display_Changing();
+        //      }
+
+    }
+    // This will make it move
+
+
+    //------------------------------------------------------------------------------
 
 }
 
 
 void Carlson_StateMachine(void){
     switch(Time_Sequence){
-      case 250:                        //
+    case 250:                        //
         if(one_time){
-          Init_LEDs();
-          lcd_BIG_mid();
-          display_changed = 1;
-          one_time = 0;
+            Init_LEDs();
+            lcd_BIG_mid();
+            display_changed = 1;
+            one_time = 0;
         }
         Time_Sequence = 0;             //
         break;
-      case 200:                        //
+    case 200:                        //
         if(one_time){
-//          P1OUT &= ~RED_LED;            // Change State of LED 4
-          P6OUT |= GRN_LED;            // Change State of LED 5
-          one_time = 0;
+            //          P1OUT &= ~RED_LED;            // Change State of LED 4
+            P6OUT |= GRN_LED;            // Change State of LED 5
+            one_time = 0;
         }
         break;
-      case 150:                         //
+    case 150:                         //
         if(one_time){
-          P1OUT |= RED_LED;            // Change State of LED 4
-          P6OUT &= ~GRN_LED;            // Change State of LED 5
-          one_time = 0;
+            P1OUT |= RED_LED;            // Change State of LED 4
+            P6OUT &= ~GRN_LED;            // Change State of LED 5
+            one_time = 0;
         }
         break;
-      case 100:                         //
+    case 100:                         //
         if(one_time){
-//          lcd_4line();
-          lcd_BIG_bot();
-          P6OUT |= GRN_LED;            // Change State of LED 5
-          display_changed = 1;
-          one_time = 0;
+            //          lcd_4line();
+            lcd_BIG_bot();
+            P6OUT |= GRN_LED;            // Change State of LED 5
+            display_changed = 1;
+            one_time = 0;
         }
         break;
-      case  50:                        //
+    case  50:                        //
         if(one_time){
-          one_time = 0;
+            one_time = 0;
         }
         break;                         //
-      default: break;
+    default: break;
     }
 
 
 }
-
-
-
-
-
