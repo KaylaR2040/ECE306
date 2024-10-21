4364054c390c320cd65d6b146daa6f7de187ad86
Kayla Radu <kaylaradu@gmail.com>
2024-10-21 19:37:25 -0400

    Began Project07

---
+++ Project07/Project03 cc/timersB0.h
@@ -0,0 +1,24 @@
+/*
+ * timersB0.h
+ *
+ *  Created on: Oct 11, 2024
+ *      Author: kaylaradu
+ */
+
+#ifndef TIMERSB0_H_
+#define TIMERSB0_H_
+#define TB0CCR0_INTERVAL (25000)
+#define TB0CCR1_INTERVAL (25000)
+#define TB0CCR2_INTERVAL (25000)
+#define WHEEL_PERIOD (50005)
+#define WHEEL_OFF (0)
+#define SLOW (35000)
+#define FAST (50000)
+#define PERCENT_100 (50000)
+#define PERCENT_80 (45000)
+
+
+
+void half_sec_delay(void);
+
+#endif /* TIMERSB0_H_ */
